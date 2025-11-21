7cf5e2d52b03e7944d95e88a1bdb8f9aa6607028
mgtm98 <m.g.t.m.prog@gmail.com>
2025-11-21 19:19:48 +0200

    Fix an issue with saving SUPER_ROOT_UUID

--- jac-scale/jac_scale/context.py
+++ jac-scale/jac_scale/context.py
@@ -1,6 +1,12 @@
-from typing import Optional
+from typing import Optional, Any
 
 from jaclang.runtimelib.machine import ExecutionContext
+from jac_scale.memory_hierarchy import MultiHierarchyMemory
+
+from dataclasses import MISSING
+from uuid import UUID
+from jaclang.compiler.constant import Constants as Con
+from jaclang.runtimelib.constructs import NodeAnchor, Root
 
 
 class JScaleExecutionContext(ExecutionContext):
@@ -12,6 +18,14 @@ class JScaleExecutionContext(ExecutionContext):
         root: Optional[str] = None,
     ) -> None:
         """Initialize JScaleExecutionContext."""
-        print("JScaleExecutionContext initialized")
-        # Call parent init which sets up mem, reports, system_root, etc.
-        super().__init__(session, root)
+        self.mem: MultiHierarchyMemory = MultiHierarchyMemory()
+        self.reports: list[Any] = []
+        self.custom: Any = MISSING
+        self.system_root = self.mem.find_by_id(UUID(Con.SUPER_ROOT_UUID))
+        if not isinstance(self.system_root, NodeAnchor):
+            self.system_root = Root().__jac__
+            self.system_root.id = UUID(Con.SUPER_ROOT_UUID)
+            self.mem.set(self.system_root)
+        self.entry_node = self.root_state = (
+            self._get_anchor(root) if root else self.system_root
+        )
